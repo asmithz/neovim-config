2c8ec38c17c1127960a0e667fd6d2e98edc205f6
asmithz <arielsmithzerega@gmail.com>
2026-04-25 11:42:15 -0400

    fix: changed telescope branch and add astro parser

--- lua/asmithz/plugins/nvim-treesitter.lua
+++ lua/asmithz/plugins/nvim-treesitter.lua
@@ -17,7 +17,8 @@ return {
                 "query", 
                 "javascript", 
                 "html", 
-                "go"
+                "go",
+                "astro"
             }
         })
     end
